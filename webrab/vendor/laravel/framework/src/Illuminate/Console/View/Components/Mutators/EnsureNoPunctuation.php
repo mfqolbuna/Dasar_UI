01e9eb499a754c98a60d84d39738b6570350cd56
Farhan Qolbuna <79250888+mfqolbuna@users.noreply.github.com>
2025-12-20 03:43:26 +0700

    Add files via upload

--- webrab/vendor/laravel/framework/src/Illuminate/Console/View/Components/Mutators/EnsureNoPunctuation.php
+++ webrab/vendor/laravel/framework/src/Illuminate/Console/View/Components/Mutators/EnsureNoPunctuation.php
@@ -4,18 +4,18 @@ namespace Illuminate\Console\View\Components\Mutators;
 
 use Illuminate\Support\Stringable;
 
-class EnsurePunctuation
+class EnsureNoPunctuation
 {
     /**
-     * Ensures the given string ends with punctuation.
+     * Ensures the given string does not end with punctuation.
      *
      * @param  string  $string
      * @return string
      */
     public function __invoke($string)
     {
-        if (! (new Stringable($string))->endsWith(['.', '?', '!', ':'])) {
-            return "$string.";
+        if ((new Stringable($string))->endsWith(['.', '?', '!', ':'])) {
+            return substr_replace($string, '', -1);
         }
 
         return $string;
