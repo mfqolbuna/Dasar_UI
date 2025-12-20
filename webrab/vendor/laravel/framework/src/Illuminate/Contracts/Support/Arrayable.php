a9e915fcea3f3abdc882ed02ae838e0e1a4608ee
Farhan Qolbuna <79250888+mfqolbuna@users.noreply.github.com>
2025-12-20 14:41:23 +0700

    Add files via upload

--- webrab/vendor/laravel/framework/src/Illuminate/Contracts/Support/Arrayable.php
+++ webrab/vendor/laravel/framework/src/Illuminate/Contracts/Support/Arrayable.php
@@ -1,1 +1,17 @@
+<?php
 
+namespace Illuminate\Contracts\Support;
+
+/**
+ * @template TKey of array-key
+ * @template TValue
+ */
+interface Arrayable
+{
+    /**
+     * Get the instance as an array.
+     *
+     * @return array<TKey, TValue>
+     */
+    public function toArray();
+}
