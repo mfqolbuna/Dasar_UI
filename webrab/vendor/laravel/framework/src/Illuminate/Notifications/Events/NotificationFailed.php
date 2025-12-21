aade764ece7f4948347d5bea0bbbc8db9707e8b5
Farhan Qolbuna <79250888+mfqolbuna@users.noreply.github.com>
2025-12-22 06:05:00 +0700

    Add files via upload

--- webrab/vendor/laravel/framework/src/Illuminate/Notifications/Events/NotificationFailed.php
+++ webrab/vendor/laravel/framework/src/Illuminate/Notifications/Events/NotificationFailed.php
@@ -5,7 +5,7 @@ namespace Illuminate\Notifications\Events;
 use Illuminate\Bus\Queueable;
 use Illuminate\Queue\SerializesModels;
 
-class NotificationSent
+class NotificationFailed
 {
     use Queueable, SerializesModels;
 
@@ -15,13 +15,13 @@ class NotificationSent
      * @param  mixed  $notifiable  The notifiable entity who received the notification.
      * @param  \Illuminate\Notifications\Notification  $notification  The notification instance.
      * @param  string  $channel  The channel name.
-     * @param  mixed  $response  The channel's response.
+     * @param  array  $data  The data needed to process this failure.
      */
     public function __construct(
         public $notifiable,
         public $notification,
         public $channel,
-        public $response = null,
+        public $data = [],
     ) {
     }
 }
--- webrab/vendor/laravel/framework/src/Illuminate/Notifications/Events/NotificationSending.php
+++ webrab/vendor/laravel/framework/src/Illuminate/Notifications/Events/NotificationSending.php
@@ -5,7 +5,7 @@ namespace Illuminate\Notifications\Events;
 use Illuminate\Bus\Queueable;
 use Illuminate\Queue\SerializesModels;
 
-class NotificationSent
+class NotificationSending
 {
     use Queueable, SerializesModels;
 
@@ -15,13 +15,11 @@ class NotificationSent
      * @param  mixed  $notifiable  The notifiable entity who received the notification.
      * @param  \Illuminate\Notifications\Notification  $notification  The notification instance.
      * @param  string  $channel  The channel name.
-     * @param  mixed  $response  The channel's response.
      */
     public function __construct(
         public $notifiable,
         public $notification,
         public $channel,
-        public $response = null,
     ) {
     }
 }
