70aaae1b62b601335cef92cca153dcfc035ba7a3
Farhan Qolbuna <79250888+mfqolbuna@users.noreply.github.com>
2025-12-23 05:42:47 +0700

    Add files via upload

--- webrab/vendor/laravel/pail/src/Contracts/Printer.php
+++ webrab/vendor/laravel/pail/src/Contracts/Printer.php
@@ -1,1 +1,13 @@
+<?php
 
+namespace Laravel\Pail\Contracts;
+
+use Laravel\Pail\ValueObjects\MessageLogged;
+
+interface Printer
+{
+    /**
+     * Prints the given message logged.
+     */
+    public function print(MessageLogged $messageLogged): void;
+}
