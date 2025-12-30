d98418f75c7315371e50098e2e817bf814c7a437
Farhan Qolbuna <79250888+mfqolbuna@users.noreply.github.com>
2025-12-30 13:44:16 +0700

    Add files via upload

--- webrab/vendor/symfony/mime/Part/Multipart/MixedPart.php
+++ webrab/vendor/symfony/mime/Part/Multipart/MixedPart.php
@@ -12,20 +12,14 @@
 namespace Symfony\Component\Mime\Part\Multipart;
 
 use Symfony\Component\Mime\Part\AbstractMultipartPart;
-use Symfony\Component\Mime\Part\MessagePart;
 
 /**
  * @author Fabien Potencier <fabien@symfony.com>
  */
-final class DigestPart extends AbstractMultipartPart
+final class MixedPart extends AbstractMultipartPart
 {
-    public function __construct(MessagePart ...$parts)
-    {
-        parent::__construct(...$parts);
-    }
-
     public function getMediaSubtype(): string
     {
-        return 'digest';
+        return 'mixed';
     }
 }
