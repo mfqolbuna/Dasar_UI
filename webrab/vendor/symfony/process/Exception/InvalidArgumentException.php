5ef464245d19a348eb651b1b478ff4a7b6632a3e
Farhan Qolbuna <79250888+mfqolbuna@users.noreply.github.com>
2025-12-30 15:04:39 +0700

    Add files via upload

--- webrab/vendor/symfony/process/Exception/InvalidArgumentException.php
+++ webrab/vendor/symfony/process/Exception/InvalidArgumentException.php
@@ -12,10 +12,10 @@
 namespace Symfony\Component\Process\Exception;
 
 /**
- * LogicException for the Process Component.
+ * InvalidArgumentException for the Process Component.
  *
  * @author Romain Neutron <imprec@gmail.com>
  */
-class LogicException extends \LogicException implements ExceptionInterface
+class InvalidArgumentException extends \InvalidArgumentException implements ExceptionInterface
 {
 }
--- webrab/vendor/symfony/process/Exception/RuntimeException.php
+++ webrab/vendor/symfony/process/Exception/RuntimeException.php
@@ -12,10 +12,10 @@
 namespace Symfony\Component\Process\Exception;
 
 /**
- * LogicException for the Process Component.
+ * RuntimeException for the Process Component.
  *
- * @author Romain Neutron <imprec@gmail.com>
+ * @author Johannes M. Schmitt <schmittjoh@gmail.com>
  */
-class LogicException extends \LogicException implements ExceptionInterface
+class RuntimeException extends \RuntimeException implements ExceptionInterface
 {
 }
